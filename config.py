bab66c78983c00726df5095ac494a44ad89d6441
Basudev29 <basud434@gmail.com>
2025-12-26 12:49:31 +0300

    Add configuration variables for bot and API keys

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+TD_API_KEY = os.getenv("TWELVEDATA_API_KEY")
+ADMIN_IDS = [123456789]  # Telegram ID of admin
+NEWS_RSS_URL = "https://www.forexnews.com/rss"  # Optional
