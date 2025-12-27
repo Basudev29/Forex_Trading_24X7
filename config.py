388c82f1fa04e0d7a86577417458033c5b38f8fa
Basudev29 <basud434@gmail.com>
2025-12-27 11:04:33 +0300

    Update admin ID in config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 TD_API_KEY = os.getenv("TWELVEDATA_API_KEY")
-ADMIN_IDS = [123456789]  # Telegram ID of admin
-NEWS_RSS_URL = "https://www.forexnews.com/rss"  # Optional
+ADMIN_IDS = [+966554382945]  # Telegram ID of admin
+
